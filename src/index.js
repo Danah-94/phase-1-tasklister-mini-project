13e81043ab5e8a355ad3840d3a352e2bda5ff45f
Danah <osta.danah@gmail.com>
2022-10-24 01:04:35 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let task = document.querySelector('#new-task-description').value
+    
+    if (task === ""){
+      return alert("Add a task before clicking create a task button");
+    }
+    
+    tasksToAdd(task)
+    form.reset();
+    
+  })
+
+  function tasksToAdd(todo){
+    let deleteBtn = document.createElement('button')
+    let li = document.createElement('li');
+    li.textContent = todo;
+    deleteBtn.innerText = ' x';
+    li.append(deleteBtn);
+    document.querySelector('#list').appendChild(li);
+
+    function deleteFunction(e){
+      e.target.parentNode.remove();
+    }
+
+    deleteBtn.addEventListener('click', deleteFunction);
+  } 
+
 });
+
